6e10818f2a089f61f11889801a49db7eea657d32
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-10 17:11:37 -0400

    added error handler to Exercise 3.2

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -17,6 +17,9 @@ const getPositionFromAddress = (address) => {
   return request(`https://api.opencagedata.com/geocode/v1/json?q=${q}&key=${key}`)
   .then((res) => JSON.parse(res))
   .then(data => data.results[0].geometry)
+  .catch((err) => {
+    return err.error ? JSON.parse(err.error) : err;
+});
 
 };
 
